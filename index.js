0b2e73e9043e5bc26ab7317c7bad6f907b13b366
Neko7sora <75793267+Neko7sora@users.noreply.github.com>
2022-01-31 21:24:21 +0900

    update preview

---
+++ index.js
@@ -0,0 +1,21 @@
+const { chromium } = require('playwright');
+console.log(__dirname);
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto("file://" + __dirname + "/_site/index.html");//GitHub actionsのときファイル移動するから注意！ "/../../_site/index.html""/_site/index.html"
+  await page.setViewportSize({
+    width: 1920,
+    height: 1080
+  })
+  await page.evaluate(async () => {
+    for (let i = 0; i < document.body.scrollHeight; i += 100) {
+      window.scrollTo(0, i);
+    }
+    window.scrollTo(0, 0);
+  });
+  await page.waitForTimeout(5000);
+  await page.screenshot({ path: __dirname + '/result.jpg', fullPage: true });
+  await browser.close();
+})()
